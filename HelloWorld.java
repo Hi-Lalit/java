f4f6267776cc2a24e700e60fbd6a9b78a47c1a7c
L.K.Gautam <hi.lkgautam@gmail.com>
2025-10-07 17:34:03 +0530

    updated by Lalit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println("Hello all, Lalit and Utkarsh");
+      System.out.println("Hello all, Lalit Kumar Gautam and Utkarsh Singh");
    }
 }
